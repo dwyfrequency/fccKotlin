d93229a340be16a78251d3ea3df4b01b31125563
Jack Dwyer <jdwy215@gmail.com>
2019-10-23 09:55:38 -0500

    adding class

--- src/Person.kt
+++ src/Person.kt
@@ -1,15 +1,25 @@
 fun main() {
     val p = Person()
     p.nickName = "Jack"
+    p.nickName = "Sweet Feet"
+    p.printInfo()
+    val p2 = Person("tom", "cav")
+    p2.printInfo()
+
 }
 
-class Person {
+class Person(val firstName: String = "Jackie", val lastName: String = "Decodes") {
     var nickName: String? = null
         set(value) {
+            val oldNickName = nickName
 //          field is keword for setting the variable
             field = value
-            println("setting field $nickName to $value")
+            println("setting field nickName from $oldNickName to $value")
         }
+
+    fun printInfo() {
+        println("$firstName $lastName ${nickName ?: "no nickname"}")
+    }
 }
 
 
